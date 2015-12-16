4a9ea080da6cc385ad88ee6de2fb7c2842f0c1ae
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-12-15 23:37:52 -0500

    add files

---
+++ events.js
@@ -0,0 +1,20 @@
+﻿(function (globalScope) {
+
+    globalScope.Events = {
+
+        on: function (obj, eventName, selector, fn) {
+
+            jQuery(obj).on(eventName, selector, fn);
+        },
+
+        off: function (obj, eventName, selector, fn) {
+
+            jQuery(obj).off(eventName, selector, fn);
+        },
+
+        trigger: function (obj, eventName, params) {
+            jQuery(obj).trigger(eventName, params);
+        }
+    };
+
+})(window);
