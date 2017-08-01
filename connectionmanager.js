9bfd548a3ad340837dcc4759ce05b738acb178ec
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-07-31 20:02:50 -0400

    bump version

--- connectionmanager.js
+++ connectionmanager.js
@@ -271,7 +271,7 @@
             return connectUser;
         };
 
-        self._minServerVersion = '3.2.14';
+        self._minServerVersion = '3.2.17';
 
         self.appVersion = function () {
             return appVersion;
