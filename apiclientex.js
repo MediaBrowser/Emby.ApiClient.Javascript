b63cb123170e7c1720710933ee6d4c682629224f
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-04-11 11:56:34 -0400

    update isLocalId

--- apiclientex.js
+++ apiclientex.js
@@ -196,7 +196,7 @@ export default class ApiClientEx extends ApiClient {
                 return Promise.resolve(result);
             });
 
-        } else if (serverInfo && options && (isLocalId(options.ParentId) || isLocalId(options.SeriesId) || isLocalId(options.SeasonId) || isLocalViewId(options.ParentId) || isLocalViewId(options.AlbumIds))) {
+        } else if (serverInfo && options && (isLocalId(options.ParentId) || isLocalId(options.SeriesId) || isLocalId(options.SeasonId) || isLocalViewId(options.ParentId) || isLocalId(options.AlbumIds))) {
 
             return this.localAssetManager.getViewItems(serverInfo.Id, userId, options).then(items => {
 
